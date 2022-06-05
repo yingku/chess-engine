f8b91260d729d8c54d9b9435c9a706b500114a3e
yingku <yingku@umich.edu>
2022-06-04 23:44:20 -0400

    including driver

--- Chess/ChessMain.py
+++ Chess/ChessMain.py
@@ -1,5 +1,5 @@
 """
-This is our main driver file. It will be responsible for handling user input and displaying the current
+This is the main driver file. It will be responsible for handling user input and displaying the current
 GameState object
 """
 
